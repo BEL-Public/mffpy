5dd69c894d2ead73c0ca1fca11e0824975bf0e4c
jusjusjus <jschwabedal@gmail.com>
2018-09-24 04:38:22 -0700

    Initial commit.

---
+++ mffpy/externals/__init__.py
@@ -0,0 +1,4 @@
+from . import decorator
+from . import funcsigs
+from . import jdcal
+from . import six
---
+++ mffpy/io/__init__.py
@@ -0,0 +1,9 @@
+"""IO module for reading raw data."""
+
+# Authors: Alexandre Gramfort <alexandre.gramfort@telecom-paristech.fr>
+#          Matti Hamalainen <msh@nmr.mgh.harvard.edu>
+#
+# License: BSD (3-clause)
+
+from . import egi
+from .egi import read_raw_egi
