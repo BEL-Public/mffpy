5aefcaf69f654a035cff0c56737f53e482c031c3
jusjusjus <jschwabedal@gmail.com>
2018-09-25 07:46:57 -0700

    Added xml readers for mff directory.
    Each mff "file" contains several xml files.  This commit
    adds an interface to each one of these files of specific
    root tags, together with tests to ensure functionality.
    
    The files and corresponding classes in `mffpy.io.egi.xml_files` are:
    
      * coordinates.xml -- `class Coordinates`
      * epochs.xml      -- `class Epochs`
      * Events_ECI.xml  -- `class Events`
      * info1.xml       -- `class DataInfo`
      * info.xml        -- `class FileInfo`
      * sensorLayout.xml-- `class SensorLayout`
      * subject.xml     -- `class Subject`

---
+++ mffpy/io/egi/xml_files.py
@@ -0,0 +1,500 @@
+"""Parsing for all xml files"""
+
+import xml.etree.ElementTree as ET
+import base64
+import numpy as np
+from os.path import splitext
+from datetime import datetime
+
+class XMLBase:
+
+    _extensions = ('.xml', '.XML')
+    _ext_err = "Unknown file type [extension has to be one of %s]"
+    _xmlns = None
+    _xmlroottag = None
+    _supported_versions = (None,)
+    _time_format = "%Y-%m-%dT%H:%M:%S.%f%z"
+
+    @classmethod
+    def _parse_time_str(cls, txt):
+        # convert time string "2003-04-17T13:35:22.000000-08:00"
+        # to "2003-04-17T13:35:22.000000-0800" ..
+        assert txt.count(':') == 3, "unexpected time string '%s'"%txt
+        txt = txt[::-1].replace(':', '', 1)[::-1] 
+        return datetime.strptime(txt, cls._time_format)
+
+    def __init__(self, filename):
+        self.filename = filename
+        self._check_ext()
+
+    def _check_ext(self):
+        assert splitext(self.filename)[1] in self._extensions, self._ext_err%self._extensions
+
+    @property
+    def _xml_root(self):
+        try:
+            return self.__xml_root
+        except AttributeError: # no attribute `__xml_root`
+            self.__xml_root = ET.parse(self.filename).getroot()
+            assert self.__xml_root.tag == self._xmlns+self._xmlroottag, "XML format in file '%s': root tag '%s' ['%s']."%(self.filename, self.__xml_root.tag, self._xmlns+self._xmlroottag)
+            self.query_version()
+            return self._xml_root
+
+    def query_version(self):
+        raise NotImplementedError
+
+    def find(self, tag, root=None):
+        root = root or self._xml_root
+        return root.find(self._xmlns+tag)
+
+    def findall(self, tag, root=None):
+        root = root or self._xml_root
+        return root.findall(self._xmlns+tag)
+
+    def nsstrip(self, tag):
+        return tag[len(self._xmlns):]
+
+
+class FileInfo(XMLBase):
+
+    _xmlns = '{http://www.egi.com/info_mff}'
+    _xmlroottag = 'fileInfo'
+    _supported_versions = ('3',)
+    
+    def query_version(self):
+        el = self.find('mffVersion')
+        self._version = None if el is None else el.text
+
+    @property
+    def version(self):
+        try:
+            return self._version
+        except AttributeError:
+            self.query_version()
+            return self.version
+
+    @property
+    def recordTime(self):
+        try:
+            return self._recordTime
+        except AttributeError:
+            self._recordTime = self._parse_recordTime()
+            return self.recordTime
+
+    def _parse_recordTime(self):
+        txt = self.find('recordTime').text
+        return self._parse_time_str(txt)
+
+
+class DataInfo(XMLBase):
+
+    _xmlns = r'{http://www.egi.com/info_n_mff}'
+    _xmlroottag = r'dataInfo'
+
+    def query_version(self):
+        return None
+
+    @property
+    def generalInformation(self):
+        try:
+            return self._generalInformation
+        except AttributeError:
+            self._generalInformation = self._parse_generalInformation()
+            return self.generalInformation
+
+    def _parse_generalInformation(self):
+        el = self.find('fileDataType', self.find('generalInformation'))
+        el = el[0]
+        info = {}
+        info['channel_type'] = self.nsstrip(el.tag)
+        for el_i in el:
+            print(el_i.tag, self.nsstrip(el_i.tag))
+            info[self.nsstrip(el_i.tag)] = el_i.text
+        return info
+
+    @property
+    def filters(self):
+        try:
+            return self._filters
+        except AttributeError:
+            self._filters = self._parse_filters()
+            return self.filters
+
+    def _parse_filters(self):
+        return [
+            self._parse_filter(f)
+            for f in self.find('filters')
+        ]
+    
+    def _parse_filter(self, f):
+        ans = {}
+        for prop in (('beginTime', float), 'method', 'type'):
+            prop, conv = prop if len(prop) == 2 else (prop, lambda x: x)
+            ans[prop] = conv(self.find(prop, f).text)
+
+        el = self.find('cutoffFrequency', f)
+        ans['cutoffFrequency'] = (float(el.text), el.get('units'))
+        return ans
+
+    @property
+    def calibrations(self):
+        try:
+            return self._calibrations
+        except AttributeError:
+            self._calibrations = self._parse_calibrations()
+            return self.calibrations
+
+    def _parse_calibrations(self):
+        calibrations = self.find('calibrations')
+        ans = {}
+        for cali in calibrations:
+            typ = self.find('type', cali)
+            ans[typ.text] = self._parse_calibration(cali) 
+        return ans
+
+    def _parse_calibration(self, cali):
+        ans = {}
+        ans['beginTime'] = float(self.find('beginTime', cali).text)
+        ans['channels'] = {
+            int(el.get('n')): np.float32(el.text)
+            for el in self.find('channels', cali)
+        }
+        return ans
+
+
+class Patient(XMLBase):
+
+    _xmlns = r'{http://www.egi.com/subject_mff}'
+    _xmlroottag = r'patient'
+
+    _type_converter = {
+        'string': str,
+        None: lambda x: x
+    }
+
+    def query_version(self):
+        return None
+
+    @property
+    def fields(self):
+        try:
+            return self._fields
+        except AttributeError:
+            self._fields = self._parse_fields()
+            return self.fields
+
+    def _parse_fields(self):
+        ans = {}
+        for field in self.find('fields'):
+            assert self.nsstrip(field.tag) == 'field', "Unknown field with tag '%s'"%self.nsstrip(field.tag)
+            name = self.find('name', field).text
+            data = self.find('data', field)
+            data = self._type_converter[data.get('dataType')](data.text)
+            ans[name] = data
+        return ans
+
+
+class SensorLayout(XMLBase):
+
+    _xmlns = r'{http://www.egi.com/sensorLayout_mff}'
+    _xmlroottag = r'sensorLayout'
+
+    _type_converter = {
+        'name': str,
+        'number': int,
+        'type': int,
+        'identifier': int,
+        'x': np.float32,
+        'y': np.float32,
+        'z': np.float32,
+    }
+
+    def query_version(self):
+        return None
+
+    @property
+    def name(self):
+        try:
+            return self._name
+        except AttributeError:
+            self._name = self.get('name').text
+            return self.name
+
+    @property
+    def sensors(self):
+        try:
+            return self._sensors
+        except AttributeError:
+            self._sensors = self._parse_sensors()
+            return self.sensors
+
+    def _parse_sensors(self):
+        return dict([
+            self._parse_sensor(sensor)
+            for sensor in self.find('sensors')
+        ])
+
+    def _parse_sensor(self, el):
+        assert self.nsstrip(el.tag) == 'sensor', "Unknown sensor with tag '%s'"%self.nsstrip(el.tag)
+        ans = {}
+        for e in el:
+            tag = self.nsstrip(e.tag)
+            ans[tag] = self._type_converter[tag](e.text)
+        return ans['number'], ans
+
+    @property
+    def threads(self):
+        try:
+            return self._threads
+        except AttributeError:
+            self._threads = self._parse_threads()
+            return self.threads
+
+    def _parse_threads(self):
+        ans = []
+        for thread in self.find('threads'):
+            assert self.nsstrip(thread.tag) == 'thread', "Unknown thread with tag '%s'"%self.nsstrip(thread.tag)
+            ans.append(tuple(map(int, thread.text.split(','))))
+        return ans
+
+    @property
+    def tilingSets(self):
+        try:
+            return self._tilingSets
+        except AttributeError:
+            self._tilingSets = self._parse_tilingSets()
+            return self.tilingSets
+
+    def _parse_tilingSets(self):
+        ans = []
+        for tilingSet in self.find('tilingSets'):
+            assert self.nsstrip(tilingSet.tag) == 'tilingSet', "Unknown tilingSet with tag '%s'"%self.nsstrip(tilingSet.tag)
+            ans.append(list(map(int, tilingSet.text.split())))
+        return ans
+
+    @property
+    def neighbors(self):
+        try:
+            return self._neighbors
+        except AttributeError:
+            self._neighbors = self._parse_neighbors()
+            return self.neighbors
+
+    def _parse_neighbors(self):
+        ans = {}
+        for ch in self.find('neighbors'):
+            assert self.nsstrip(ch.tag) == 'ch', "Unknown ch with tag '%s'"%self.nsstrip(ch.tag)
+            key = int(ch.get('n'))
+            ans[key] = list(map(int, ch.text.split()))
+        return ans
+
+    @property
+    def mappings(self):
+        try:
+            return self._mappings
+        except AttributeError:
+            self._mappings = self._parse_mappings()
+            return self.mappings
+
+    def _parse_mappings(self):
+        raise NotImplementedError("No method to parse mappings.")
+
+
+class Coordinates(XMLBase):
+
+    _xmlns = r'{http://www.egi.com/coordinates_mff}'
+    _xmlroottag = r'coordinates'
+    _type_converter = {
+        'name': str,
+        'number': int,
+        'type': int,
+        'identifier': int,
+        'x': np.float32,
+        'y': np.float32,
+        'z': np.float32,
+    }
+
+    def query_version(self):
+        return None
+
+    @property
+    def acqTime(self):
+        try:
+            return self._acqTime
+        except AttributeError:
+            self._acqTime = self._parse_acqTime()
+            return self.acqTime
+
+    def _parse_acqTime(self):
+        txt = self.find("acqTime").text
+        return self._parse_time_str(txt)
+
+    @property
+    def acqMethod(self):
+        try:
+            return self._acqMethod
+        except AttributeError:
+            self._acqMethod = self._parse_acqMethod()
+            return self.acqMethod
+
+    def _parse_acqMethod(self):
+        el = self.find("acqMethod")
+        return el.text
+
+    @property
+    def name(self):
+        try:
+            return self._name
+        except AttributeError:
+            self._name = self.get('name').text
+            return self.name
+
+    @property
+    def defaultSubject(self):
+        try:
+            return self._defaultSubject
+        except AttributeError:
+            self._defaultSubject = bool(self.find('defaultSubject').text)
+            return self.defaultSubject
+
+    @property
+    def sensors(self):
+        try:
+            return self._sensors
+        except AttributeError:
+            self._sensors = self._parse_sensors()
+            return self.sensors
+
+    def _parse_sensors(self):
+        sensorLayout = self.find('sensorLayout')
+        return dict([
+            self._parse_sensor(sensor)
+            for sensor in self.find('sensors', sensorLayout)
+        ])
+
+    def _parse_sensor(self, el):
+        assert self.nsstrip(el.tag) == 'sensor', "Unknown sensor with tag '%s'"%self.nsstrip(el.tag)
+        ans = {}
+        for e in el:
+            tag = self.nsstrip(e.tag)
+            ans[tag] = self._type_converter[tag](e.text)
+        return ans['number'], ans
+
+
+class Epochs(XMLBase):
+
+    _xmlns = r'{http://www.egi.com/epochs_mff}'
+    _xmlroottag = r'epochs'
+    _type_converter = {
+        'beginTime': int,
+        'endTime': int,
+        'firstBlock': int,
+        'lastBlock': int,
+    }
+
+    def query_version(self):
+        return None
+
+    @property
+    def epochs(self):
+        try:
+            return self._epochs
+        except AttributeError:
+            self._epochs = self._parse_epochs()
+            return self.epochs
+
+    def _parse_epochs(self):
+        return [
+            self._parse_epoch(epoch)
+            for epoch in self._xml_root
+        ]
+
+    def _parse_epoch(self, el):
+        assert self.nsstrip(el.tag) == 'epoch', "Unknown epoch with tag '%s'"%self.nsstrip(el.tag)
+        ans = {}
+        for e in el:
+            tag = self.nsstrip(e.tag)
+            ans[tag] = self._type_converter[tag](e.text)
+        return ans
+
+
+class EventTrack(XMLBase):
+
+    _xmlns = r'{http://www.egi.com/event_mff}'
+    _xmlroottag = r'eventTrack'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._event_type_converter = {
+            'beginTime': lambda e: self._parse_time_str(e.text),
+            'duration': lambda e: int(e.text),
+            'code': lambda e: str(e.text),
+            'label': lambda e: str(e.text),
+            'description': lambda e: str(e.text),
+            'sourceDevice': lambda e: str(e.text),
+            'keys': self._parse_keys
+        }
+        self._key_type_converter = {
+            'short': np.int16,
+            'string': str,
+        }
+
+    def query_version(self):
+        return None
+
+    @property
+    def name(self):
+        try:
+            return self._name
+        except AttributeError:
+            self._name = self.find('name').text
+            return self.name
+
+    @property
+    def trackType(self):
+        try:
+            return self._trackType
+        except AttributeError:
+            self._trackType = self.find('trackType').text
+            return self.trackType
+
+    @property
+    def events(self):
+        try:
+            return self._events
+        except AttributeError:
+            self._events = self._parse_events()
+            return self.events
+
+    def _parse_events(self):
+        return [
+            self._parse_event(event)
+            for event in self.findall('event')
+        ]
+
+    def _parse_event(self, events_el):
+        assert self.nsstrip(events_el.tag) == 'event', "Unknown event with tag '%s'"%self.nsstrip(events_el.tag)
+        return {
+            tag: self._event_type_converter[tag](el)
+            for tag, el in map(lambda e: (self.nsstrip(e.tag), e), events_el)
+        }
+
+    def _parse_keys(self, keys_el):
+        return dict([self._parse_key(key_el)
+            for key_el in keys_el])
+
+    def _parse_key(self, key):
+        """
+        Attributes :
+            key (ElementTree.XMLElement) : parsed from a structure 
+                ```
+                <key>
+                    <keyCode>cel#</keyCode>
+                    <data dataType="short">1</data>
+                </key>
+                ```
+        """
+        code = self.find('keyCode', key).text
+        data = self.find('data', key)
+        val = self._key_type_converter[data.get('dataType')](data.text)
+        return code, val 
