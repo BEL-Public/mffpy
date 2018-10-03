1ffa5ae56519e2ed5da937a5bd249bc2abb1232d
jusjusjus <jschwabedal@gmail.com>
2018-10-03 08:16:29 -0700

    Fixed Epoch handling by read function.
    `xml_files.Epoch` contains time/duration information and block indices.
    The block indices are now used to access the data, whereas the time
    information is, informational with regards to a discontinuous .mff file.
    
    Furthermore, we fixed the RawBinFile data reading test.  Issue was that
    `A` was not reindexed correctly as `np.searchsorted`-kwarg `side` was
    changed from `left` to `right`.

--- mffpy/raw_bin_files.py
+++ mffpy/raw_bin_files.py
@@ -99,10 +99,18 @@ class RawBinFile:
         additional header pre-appended if the flag equals `1`, else it's
         only a data block.
         """
-        return self.read(self._flag_format) == 1
+        self._current_header_flag = self.read(self._flag_format)
+        return self._current_header_flag == 1
 
     def _read_header_block(self):
+        """returns a header block read from the current file pointer position.
+
+        To Do:
+        * What is `sigoffset` and `sigoffset2`?  How is it used?
+        """
         keys = ('header_size', 'block_size', 'num_channels')
+        # prior we already read 1 * 4 bytes (flag)
+        # read 3 * 4 bytes (4 * 4bytes):
         header = dict(zip(keys, self.read('3i')))
         # number of 4-byte samples in the data block
         hl = header['block_size']//4
@@ -111,8 +119,10 @@ class RawBinFile:
         # number of samples per channel in the data block
         num_samples = hl//nc
         channel_fmt = str(header['num_channels'])+'i'
-        sigoffset = self.read(channel_fmt)
-        sigfreq = self.read(channel_fmt)
+        # read nc * 4bytes ((4+nc) * 4bytes):
+        sigoffset = self.read(channel_fmt) # read 1 * nc * 4 bytes
+        # read nc * 4bytes ((4+2*nc) * 4bytes):
+        sigfreq = self.read(channel_fmt) # read 1 * nc * 4 bytes
         depth = sigfreq[0] & 0xFF
         assert depth == 32, 'Unable to read MFF with `depth != 32` [`depth=%s`]'%depth
         sampling_rate = sigfreq[0] >> 8
@@ -143,7 +153,7 @@ class RawBinFile:
                 sampling_rate.append(header['sampling_rate'])
                 num_channels.append(header['num_channels'])
             else:
-                assert header is not None, "First block must be a header."
+                assert header is not None, "First block must be a header.  Header flag was: %s"%self._current_header_flag
                 num_samples_by_block.append(header['num_samples'])
 
             self.data_blocks.append(DataBlock(self.tell(), header['block_size']))
@@ -171,36 +181,48 @@ class RawBinFile:
                     [0]+self.signal_blocks['num_samples_by_block'])
         return self.block_start_idx
 
-    def read_raw_samples(self, t0=0.0, dt=None):
-        """return `(channels, samples)`-array of data
-        
-        Data contains all data within the time range `t0, t0+dt`
-        rounded evenly.
-        """
-        sr = self.signal_blocks['sampling_rate']
-        nc = self.signal_blocks['num_channels']
-        a0_blk = self.block_start_idx
-        # Calculate ...
-        # ... the sample index of `t0` and `t0+dt`
-        a = np.round(t0*sr).astype(int)
-        b = np.round((t0+dt)*sr).astype(int) if dt is not None else None
-        # ... the block index enclosing `a` and `b`
-        A = a0_blk.searchsorted(a, side='right')-1
-        B = a0_blk.searchsorted(b, side='left') if b is not None else len(a0_blk)-1
-        # ... the relative offset into readout blocks
-        a -= a0_blk[A]
-        b = b-a0_blk[A] if b is not None else None
+    def _read_blocks(self, A, B, num_channels):
 
         def read_block(block):
             self.seek(block.byte_offset)
-            return np.frombuffer(
-                    self.file.read(block.byte_size), '<f4',
-                    count=-1).reshape(nc, -1, order='C')
+            buf = self.file.read(block.byte_size)
+            data = np.frombuffer(buf, '<f4', count=-1)
+            return data.reshape(num_channels, -1, order='C')
 
-        block_data = np.concatenate([
+        return np.concatenate([
             read_block(self.data_blocks[i])
             for i in range(A, B)
         ], axis=1)
 
-        block_data = block_data[:, a:b]
-        return block_data
+    def read_raw_samples(self, t0=None, dt=None, block_slice=None):
+        """return `(channels, samples)`-array of data
+
+        Data contains all data within the time range `t0, t0+dt`
+        relative to the block slice.
+        """
+        # The data is organized in unequal-sized data blocks that may enclose
+        # epochs of continuous recordings with breaks.  `block_slice` indexes
+        # into encloses such epochs if not None, but we might want only a small
+        # chunk of it given by `t0` and `dt`.  Therefore, we further index into
+        # blocks `bsi` selected through block_slice with the variables `A` and
+        # `B`.  Block indices `A` and `B` are chosen to enclose the interval
+        # `(t0, t0+dt)` which we would like to read.
+        assert block_slice is None or isinstance(block_slice, slice)
+        block_slice = block_slice if block_slice else slice(0, len(self.block_start_idx)-1)
+        bsi = self.block_start_idx[block_slice]
+        sr = self.signal_blocks['sampling_rate']
+        nc = self.signal_blocks['num_channels']
+        # Calculate ...
+        # ... the relative sample index of `t0` and `t0+dt`
+        a = np.round(t0*sr).astype(int) if t0 is not None else None
+        b = np.round((t0+dt)*sr).astype(int) if dt is not None else None
+        # ... the (relative) block index enclosing `bsi[0]+a` and `bsi[0]+b`
+        A = bsi.searchsorted(bsi[0]+a, side='right')-1 if a is not None  else 0
+        B = bsi.searchsorted(bsi[0]+b, side='left') if b is not None else len(bsi)
+        # ... the (absolute) block index enclosing <..>
+        A += block_slice.start
+        B += block_slice.start
+        # Access the file to read the data:
+        block_data = self._read_blocks(A, B, nc)
+        # Reject relative offsets into readout blocks
+        return block_data[:, a:b]
