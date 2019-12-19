db2a3974a755d8480cf0f7f227444990210ca166
Wayne Manselle <wayne.manselle@belco.tech>
2019-12-18 17:17:40 -0800

    This commit increases the mff2json functionality. This commit modifies the mff2json script to operate on both a single MFF or a directory of MFFs

--- bin/mff2json.py
+++ bin/mff2json.py
@@ -1,4 +1,10 @@
 """
+This script is used to export the contents of segmented MFF files into a JSON format using mffpy.
+It will convert either a single MFF file, or a directory of MFF files.
+
+@author: Damian Persico - persicodamian@gmail.com
+@author: Wayne Manselle - wayne.manselle@belco.tech
+
 Copyright 2019 Brain Electrophysiology Laboratory Company LLC
 
 Licensed under the ApacheLicense, Version 2.0(the "License");
@@ -12,38 +18,50 @@ distributed under the License is distributed on an
 "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 ANY KIND, either express or implied.
 """
-
-"""
-This script is used to export the contents of an MFF file into a JSON format.
-
-@author: Damian Persico - persicodamian@gmail.com
-"""
-
 from os.path import splitext, sep
 from mffpy import Reader, Writer
 from argparse import ArgumentParser
 
+import glob
 
-def mff2json(input_filename):
-    if input_filename:
+
+def mff2json(input_path):
+    """
+    This function undertakes the work of effectuating the conversions of a passed MFF file, or set of MFF files into
+    a JSON format.
+
+    These JSON formatted conversions will be saved in the same directory as the MFF(s) converted.
+    :param input_path: A string describing a path to a specific MFF, or a collection of them.
+    :return:
+    """
+
+    file_list = []
+
+    if '.mff' in input_path:
+        file_list = [input_path]
+    else:
+        file_list = glob.glob(''.join([input_path, '*.mff']))
+
+    print(file_list)
+
+    for file in file_list:
         # Check .mff input
-        dir_and_base, ext = splitext(input_filename.rstrip(sep))
-        assert ext.lower() == '.mff', f"{input_filename} is not a valid .mff directory"
+        dir_and_base, ext = splitext(file.rstrip(sep))
+        # assert ext.lower() == '.mff', f"{input_path} is not a valid .mff directory"
         output_filename = dir_and_base + '.json'
 
         # Read data from an MFF file
-        reader = Reader(input_filename)
-        print("Reading data from " + str(input_filename))
+        reader = Reader(file)
+        print("Reading data from " + str(file))
         data = reader.get_mff_content()
 
         # Write data into a JSON file
         writer = Writer(output_filename)
         print("Writing data out to " + str(output_filename))
         writer.export_to_json(data)
-    else:
-        print("No .mff file provided.\nPlease, provide a path to a valid .mff directory")
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--mff_file", type=str, help="Path to an MFF directory")
+    parser.add_argument("--mff_file", type=str, help="Path to an MFF File, or Collection of MFF Files")
     mff2json(parser.parse_args().mff_file)
