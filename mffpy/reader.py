f043d0e64b0f7e81041dd8c5929cd81c35154819
Evan Hathaway <evan.hathaway@bel.company>
2020-09-02 18:56:14 -0700

    Clean up and documentation
    Here we get rid of a couple of unnecessary
    functions in `mffpy.reader` that have been
    moved to `mffpy.xml_files`.
    
    We add a function `Categories.sort_categories_by_starttime`
    to give us a list of sorted blocks with category
    names. We use this function when associating
    categories and epochs.
    
    We also add some docstrings and tests.

--- mffpy/reader.py
+++ mffpy/reader.py
@@ -20,7 +20,7 @@ import numpy as np
 from cached_property import cached_property
 
 from . import xml_files
-from .xml_files import XML, Epochs
+from .xml_files import XML, Categories, Epochs
 from . import bin_files
 from .mffdir import get_directory
 from base64 import b64encode
@@ -61,7 +61,7 @@ class Reader:
         self.directory = get_directory(filename)
 
     @cached_property
-    def categories(self) -> Dict:
+    def categories(self) -> Categories:
         """
         ```python
         Reader.categories
@@ -76,27 +76,7 @@ class Reader:
             categories = XML.from_file(fp)
         assert isinstance(categories, xml_files.Categories), f"""
             .xml file 'categories.xml' of wrong type {type(categories)}"""
-        return categories.categories
-
-    def _sort_categories_by_starttime(self, categories: Dict) -> List[Dict]:
-        """
-        sort category info for each block of data by start time
-
-        Extract category and start time info for each block in `categories`
-        and put into a list. Return the list sorted by start time of each
-        block.
-
-        **Arguments**
-
-        * **`categories`**: `Dict` containing category info. Can be retrieved
-        with `self.categories`.
-        """
-        cat_list = []
-        for cat in categories.keys():
-            for block in categories[cat]:
-                cat_list.append({'category': cat, 't0': block['beginTime']})
-        cat_list.sort(key=lambda b: b['t0'])
-        return cat_list
+        return categories
 
     @cached_property
     def epochs(self) -> Epochs:
@@ -124,36 +104,6 @@ class Reader:
         # Sort category info by start time of each block
         epochs.associate_categories(categories)
         return epochs
-
-    def epochs_by_name(self, name):
-        """
-        return all epochs with name `name`
-
-        Return a list of epochs where `Epoch.name` matches `name`.
-        If only one epoch matches, return the `Epoch` object itself.
-
-        **Arguments**
-
-        * **`name`**: `str` name of desired epochs. If working with
-        a segmented or averaged .mff, name is synonymous with the
-        corresponding category from `self.categories`.
-
-        **Example use**
-
-        ```python
-        import mffpy
-        fo = mffpy.Reader('./examples/example_4.mff')
-        fo.epochs_by_name('Category A')
-        ```
-        """
-        matched_epochs = []
-        for ep in self.epochs:
-            if ep.name == name:
-                matched_epochs.append(ep)
-        if len(matched_epochs) == 1:
-            return matched_epochs[0]
-        else:
-            return matched_epochs
 
     @cached_property
     def sampling_rates(self) -> Dict[str, float]:
