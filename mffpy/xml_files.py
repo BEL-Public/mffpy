d876a2f1948ad82a6bc11ccc5986c6886cf63ce1
Justus Schwabedal <jschwabedal@gmail.com>
2021-01-07 10:09:48 +0100

    Add(FileInfo): doc strings to acquisitionVersion and ampType

--- mffpy/xml_files.py
+++ mffpy/xml_files.py
@@ -167,11 +167,13 @@ class FileInfo(XML):
 
     @cached_property
     def acquisitionVersion(self):
+        """return content of acquisitionVersion field"""
         el = self.find('acquisitionVersion')
         return None if el is None else el.text
 
     @cached_property
     def ampType(self):
+        """return content of ampType field"""
         el = self.find('ampType')
         return None if el is None else el.text
 
@@ -185,7 +187,7 @@ class FileInfo(XML):
                 mffVersion: str = '3',
                 acquisitionVersion: str = None,
                 ampType: str = None) -> dict:
-        """returns mffVersion and time of recording start
+        """returns MFF file information
 
         As version we only provide '3' at this time.  The time has to provided
         as a `datetime.datetime` object.
@@ -209,7 +211,14 @@ class FileInfo(XML):
         return content
 
     def get_content(self):
-        """return mff version and time of recording start"""
+        """return dictionary of MFF file information
+
+        The info are comprised of:
+        - MFF version number
+        - time of start of recording
+        - acquisition version (optional)
+        - amplifier type (optional)
+        """
         content = {
             'mffVersion': self.mffVersion,
             'recordTime': self.recordTime
@@ -223,8 +232,7 @@ class FileInfo(XML):
         return content
 
     def get_serializable_content(self):
-        """return a serializable object containing the
-        mff version and time of recording start"""
+        """return serializable dictionary of MFF file information"""
         content = copy.deepcopy(self.get_content())
         content['recordTime'] = XML._dump_datetime(content['recordTime'])
         return content
