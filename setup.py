0447a38a27c3ea059107590851b3be644c41318e
Justus Schwabedal <jschwabedal@gmail.com>
2021-02-05 20:50:22 +0100

    Fix(setup.py): rm pip import w/ breaks 'build'

--- setup.py
+++ setup.py
@@ -1,49 +1,5 @@
 from os.path import join
-import pip
 import setuptools
-
-
-def v1_ge_v2(module, version):
-    """return `module.__version__ >= version`"""
-
-    def v2li(v):
-        """`v2li('20.0') == [20, 0]`"""
-        return list(map(int, v.split('.')))
-
-    version = v2li(version)
-    module = v2li(module)
-    # add semver zeros if, e.g., version='20.5.1' and module='20.5'
-    if len(version) > len(module):
-        module += [0] * (len(version) - len(module))
-
-    # Compare version numbers
-    for m, v in zip(module, version):
-        if m > v:
-            # current SEMVER number larger
-            return True
-        elif m == v:
-            # go to next number
-            continue
-        # skip `else` case
-        break
-    else:
-        # Version numbers exactly equal
-        return True
-    # Indeed, `module.__version__ < version`
-    return False
-
-
-# parse "./requirements.txt" using pip's `parse_requirements`
-if v1_ge_v2(pip.__version__, '10'):
-    from pip._internal.req import parse_requirements
-else:
-    from pip.req import parse_requirements
-
-install_reqs = parse_requirements('requirements.txt', session=False)
-if v1_ge_v2(pip.__version__, '20.1'):
-    requirements = [str(ir.requirement) for ir in install_reqs]
-else:
-    requirements = [str(ir.req) for ir in install_reqs]
 
 # We expect "mffpy/version.py" to be very simple:
 #
@@ -53,6 +9,7 @@ else:
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package)
 __version__ = ''
 exec(open(join('mffpy', 'version.py')).read())
+requirements = open('requirements.txt').read().split('\n')
 
 setuptools.setup(
     name='mffpy',
