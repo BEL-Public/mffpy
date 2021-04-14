d61c27c164a57240f619230e8d926d1ee910f787
Justus Schwabedal <jschwabedal@gmail.com>
2021-04-14 16:09:33 +0200

    Chg(mffdiff): ref. code, add docs

--- bin/mffdiff.py
+++ bin/mffdiff.py
@@ -9,6 +9,8 @@ from subprocess import check_output
 from mffpy import Reader
 from argparse import ArgumentParser
 
+char_limit = 50
+
 parser = ArgumentParser(description="""
 compare the contents of two MFF files
 """)
@@ -20,15 +22,31 @@ left_mff = Reader(args.leftpath)
 right_mff = Reader(args.rightpath)
 
 
-def getnested(cls, props, callback=None):
+def getnested(instance, props, callback=None):
+    """get nested property of instance
+
+    This is a recursive function to access a nested property, e.g. for
+    `instance.a.b.c`, props is `['a', 'b', 'c']`.  `callback` is optionally
+    applied to the final value.
+
+    Parameters
+    ----------
+        instance:
+            instance that has a property `props[0]`
+        props:
+            nested property split into a list
+        callback:
+            is optionally applied to the output value
+    """
     if props:
-        val = getattr(cls, props[0])
+        val = getattr(instance, props[0])
         return getnested(val, props[1:], callback)
 
-    return callback(cls) if callback else cls
+    return callback(instance) if callback else instance
 
 
 def compare_raw(filename):
+    """compare file in MFF using `$ diff`"""
     leftfile = join(args.leftpath, filename)
     rightfile = join(args.rightpath, filename)
     try:
@@ -40,13 +58,24 @@ def compare_raw(filename):
     print(f">>> {status} @ file '{filename}'")
 
 
-def compare(prop, callback=None, info=''):
+def compare(prop: str, callback=None, info: str = ''):
+    """compare property between left_mff and right_mff
+
+    Parameters
+    ----------
+        prop:
+            string specifying a nested property of `mffpy.Reader` instance
+        callback:
+            post processing of the value of `prop`
+        info:
+            additional note of the nested property
+    """
     props = prop.split('.')
     try:
         left_value = getnested(left_mff, props, callback)
         right_value = getnested(right_mff, props, callback)
-        left_msg = str(left_value)[:50]
-        right_msg = str(right_value)[:50]
+        left_msg = str(left_value)[:char_limit]
+        right_msg = str(right_value)[:char_limit]
         if left_value != right_value:
             msg = f'\t{left_msg} != {right_msg}'
             status = 'diff'
@@ -56,7 +85,7 @@ def compare(prop, callback=None, info=''):
 
     except BaseException as err:
         status = 'error'
-        msg = str(err)[:50] + '\n'
+        msg = str(err)[:char_limit] + '\n'
         left_value = None
 
     print(f">>> {status} @ reader_instance.{prop} "
@@ -67,14 +96,13 @@ def compare(prop, callback=None, info=''):
     return left_value if status == 'match' else None
 
 
+simple_props = ['flavor', 'sampling_rates', 'durations', 'startdatetime',
+                'units', 'num_channels', 'categories.categories']
+
 print(f"Comparing {args.leftpath} with {args.rightpath}")
-compare('flavor')
-compare('sampling_rates')
-compare('durations')
-compare('startdatetime')
-compare('units')
-compare('num_channels')
-compare('categories.categories')
+for prop in simple_props:
+    compare(prop)
+
 epoch_count = compare('epochs.epochs', lambda e: len(e), 'len(..)')
 if epoch_count is not None:
     for i in range(epoch_count):
